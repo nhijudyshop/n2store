5190ed42b99cd9d44247f30ab05aaf2964d0333d
zxGreenxz <nguyentamca165@gmail.com>
2026-01-10 08:46:29 +0700

    delete customer 360

--- unified-customer-hub/src/config/index.ts
+++
@@ -1,3 +0,0 @@
-export { env, validateEnv } from './env.js';
-export { db, query, transaction, healthCheck, shutdown, pool } from './database.js';
-export * from './constants.js';
--- unified-customer-hub/src/modules/wallet/index.ts
+++
@@ -1,6 +0,0 @@
-export { walletService, deposit, withdraw, issueVirtualCredit, getWallet, getBalance } from './wallet.service.js';
-export { walletController } from './wallet.controller.js';
-export { default as walletRoutes } from './wallet.routes.js';
-export { WalletError, WALLET_ERROR_CODES } from './wallet.errors.js';
-export * from './wallet.types.js';
-export * from './wallet.schema.js';
