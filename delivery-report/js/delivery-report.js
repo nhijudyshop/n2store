627cb4ce79c0207f8ccbe956a1e9a28decc8c4d8
nhijudyshop <147459165+nhijudyshop@users.noreply.github.com>
2026-04-02 18:52:40 +0700

    Revert "fix(delivery-report): only show orders with StateCode CrossCheckComplete"
    This reverts commit 79d353cdc1da41482247ead8ea421f6a9cd6a713.

--- delivery-report/js/delivery-report.js
+++ delivery-report/js/delivery-report.js
@@ -215,7 +215,7 @@
             }
 
             const result = await response.json();
-            DeliveryReportState.allData = (result.value || []).filter(item => item.StateCode === 'CrossCheckComplete');
+            DeliveryReportState.allData = result.value || [];
 
             // Debug: check DeliveryNote field
             const withNote = DeliveryReportState.allData.filter(i => i.DeliveryNote);
